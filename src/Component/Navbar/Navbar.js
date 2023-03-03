edc9728d201b2ab08c3ddaa1f719b6ee6d5cfa2a
MD-ASHARUDDIN-90 <asharansari90@gmail.com>
2023-03-03 21:11:39 +0530

    Fixture file modified

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,83 +1,95 @@
 import { useState } from "react";
+import { AiOutlineHighlight } from "react-icons/ai";
+import { GoTextSize } from "react-icons/go";
+import { MdOutlineFormatColorText } from "react-icons/md";
 import { icons, fontSizeList, fontFamilyList } from "../../Fixture/Icons";
 import style from "./Navbar.module.css";
 export default function Navbar() {
-  const [showColor, setShowColor] = useState(false);
-  const [showFont, setShowFont] = useState(false);
-  const [showFontName, setShowFontName] = useState(false);
- 
-  const [fontName, setFontName] = useState("Arial");
-
   const [fontSize, setFontSize] = useState(3);
+  const [fontName, setFontName] = useState("Font Style");
   const [color, setColor] = useState("black");
+  const [higlightColor, setHiglightColor] = useState("black");
+
+
 
   function handleAction(element) {
-    console.log(element.action);
-    if (element.action === "foreColor") {
-      setShowColor(!showColor ? true : false);
-      document.execCommand("foreColor", false, color);
-    } else if (element.action === "fontSize") {
-      setShowFont(!showFont ? true : false);
-      document.execCommand("fontSize", false, fontSize);
-    } else if (element.action === "backColor") {
-      setShowColor(!showColor ? true : false);
-      document.execCommand("backColor", false, color);
-    } else if (element.action === "fontName") {
-      setShowFontName(!showFontName ? true : false);
-      document.execCommand("fontName", false, fontName);
-    }else if (element.action === "insertImage") {
-        setShowFontName(!showFontName ? true : false);
-        document.execCommand("insertImage", false, );
-      }else {
-      document.execCommand(`${element.action}`);
-    }
+    document.execCommand(`${element.action}`);
+  }
+  function handleFontColor(e) {
+    setColor(e.target.value);
+    document.execCommand("foreColor", false, e.target.value);
+  }
+  function handleFontSize(e) {
+    setFontSize(e.target.value);
+    document.execCommand("fontSize", false, e.target.value);
+  }
+  function handleHighlightColor(e) {
+    setHiglightColor(e.target.value);
+    document.execCommand("backColor", false, e.target.value);
+  }
+  function handleFontStyle(e) {
+    setFontName(e.target.value);
+    document.execCommand("fontName", false, e.target.value);
+    console.log(e.target.value);
   }
 
   return (
     <div className={style.wrapper}>
-      {icons.map((element, index) => (
+      {icons.slice(0, 4).map((element, index) => (
         <button key={index} onClick={() => handleAction(element)}>
           {element.icon}
         </button>
       ))}
 
-      <div>
-        {showColor ? (
-          <input
-            type="color"
-            value={color}
-            onChange={(e) => setColor(e.target.value)}
-          />
-        ) : (
-          ""
-        )}
-        {showFont ? (
-          <div>
-            <select onChange={(e) => setFontSize(e.target.value)}>
-              <option>FontSize</option>
-              {fontSizeList.map((x) => (
-                <option key={x}>{x}</option>
-              ))}
-            </select>
-          </div>
-        ) : (
-          ""
-        )}
-    
-        {showFontName ? (
-          <div>
-            <select onChange={(e) => setFontName(e.target.value)}>
-              <option>{fontName}</option>
-              {fontFamilyList.map((x) => (
-                <option key={x}>{x}</option>
-              ))}
-            </select>
-          </div>
-        ) : (
-          ""
-        )}
-       
-        </div>
+      <button>
+        <label htmlFor="color">
+          <MdOutlineFormatColorText />
+        </label>
+        <input
+          id="color"
+          type="color"
+          value={color}
+          onChange={handleFontColor}
+        />
+      </button>
+
+      <button>
+        <label htmlFor="fontSize">
+          <GoTextSize />
+        </label>
+        <select id="fontSize" onChange={handleFontSize}>
+          <option>3</option>
+          {fontSizeList.map((x) => (
+            <option key={x}>{x}</option>
+          ))}
+        </select>
+      </button>
+
+      <button>
+        <label htmlFor="highlighColor">
+          <AiOutlineHighlight />
+        </label>
+        <input
+          id="highlighColor"
+          type="color"
+          value={higlightColor}
+          onChange={handleHighlightColor}
+        />
+      </button>
+
+      <button>
+        <select id="fontStyle" onChange={handleFontStyle}>
+          <option>{fontName}</option>
+          {fontFamilyList.map((x) => (
+            <option key={x}>{x}</option>
+          ))}
+        </select>
+      </button>
+      {icons.slice(8).map((element, index) => (
+        <button key={index} onClick={() => handleAction(element)}>
+          {element.icon}
+        </button>
+      ))}
     </div>
   );
 }
